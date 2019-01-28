f7c1de67157803115c9b99d8fb602c0a6e6386df
1705990245 <13991034472@139.com>
2019-01-28 12:21:08 +0800

    修改

--- 模拟实现strcpy(19_1_20)/模拟实现strcpy/my_strcpy.c
+++ 模拟实现strcpy(19_1_20)/模拟实现strcpy/my_strcpy.c
@@ -1,13 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+#include<assert.h>
+
+//char *strcpy(char *strDestination, const char *strSource);
+
+char* my_strcpy(char* dest, const char* src)
+{
+	assert(dest);
+	assert(src);
+	char* ptr = dest;
+	while (*src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = *src;
+	return ptr;
+}
 int main()
 {
-	char s1 = "ab";
-	char s2 = "cdefgi";
-	strcpy(s1, s2);
-	printf("%s\n", s1);
+	char s1[] = "abefg";
+	char s2[] = "cdefg";
+	printf("%s\n", my_strcpy(s1, s2));
 	system("pause");
 	return 0;
 }
