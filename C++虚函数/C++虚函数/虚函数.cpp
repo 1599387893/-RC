bcfae2bafb9acc70ce6b436606c56990f2b52d27
1705990245 <13991034472@139.com>
2019-02-24 19:49:39 +0800

    添加(2.24)

--- C++虚函数/C++虚函数/虚函数.cpp
+++ C++虚函数/C++虚函数/虚函数.cpp
@@ -1,23 +1,115 @@
+//////#include<iostream>
+//////using namespace std;
+//////
+//////class Base
+//////{
+//////public:
+//////	virtual void f() {	cout << "调用Base::f()" << endl; }
+//////};
+//////class Derived :public Base
+//////{
+//////public:
+//////	void f(){ cout << "调用Derived::f()" << endl; }
+//////};
+//////
+//////int main()
+//////{
+//////	Derived d1;
+//////	d1.f();
+//////	Base *p = &d1;
+//////	p->f();
+//////	system("pause");
+//////	return 0;
+//////}
+////
+////
+////#include<iostream>
+////using namespace std;
+////
+////class Base
+////{
+////public:
+////	virtual void Show()const { cout << "调用Base::Show" << endl; }
+////};
+////class Derived :public Base
+////{
+////public:
+////	void Show()const { cout << "调用Derived::Show" << endl; }
+////};
+////
+////void Refers(const Base& obj)
+////{
+////	obj.Show();
+////	return;
+////}
+////int main()
+////{
+////	Base obj1;
+////	Derived obj2;
+////	Refers(obj1);
+////	Refers(obj2);
+////	system("pause");
+////	return 0;
+////}
+//
+//#include<iostream>
+//using namespace std;
+//
+//class Base
+//{
+//private:
+//	int m;
+//public:
+//	Base(int a) :m(a){}
+//	virtual void Show()const { cout << m << endl; }
+//};
+//class Derived :public Base
+//{
+//private:
+//	int n;
+//public:
+//	Derived(int a, int b) :Base(a), n(b){}
+//	void Show() const
+//	{
+//		cout << n << ",";
+//		Base::Show();
+//	}
+//};
+//
+//int main()
+//{
+//	Base obj1(168);
+//	Derived obj2(158, 98);
+//	Base* p;
+//	p = &obj1;
+//	p->Show();
+//	p = &obj2;
+//	p->Show();
+//	p->Base::Show();
+//	system("pause");
+//	return 0;
+//}
+
 #include<iostream>
 using namespace std;
 
-class Base
+class A
 {
 public:
-	virtual void f() {	cout << "调用Base::f()" << endl; }
+	virtual void Show()const { cout << "基类A" << endl; }
 };
-class Derived :public Base
+class B :public A
 {
 public:
-	void f(){ cout << "调用Derived::f()" << endl; }
+	void Show()const { cout << "派生类B" << endl; }
 };
 
 int main()
 {
-	Derived d1;
-	d1.f();
-	Base *p = &d1;
-	p->f();
+	B obj;
+	A* p = &obj;
+	p->Show();
+
 	system("pause");
 	return 0;
 }
