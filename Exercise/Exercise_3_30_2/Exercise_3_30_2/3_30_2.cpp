5155ed1a80c87c24887f7f6f2f220dbdd3d9b53f
1705990245 <13991034472@139.com>
2019-03-30 22:25:12 +0800

    提交代码(3.30)

---
+++ Exercise/Exercise_3_30_2/Exercise_3_30_2/3_30_2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode
+{
+	ListNode* _next;
+	ListNode* _prev;
+
+	void* operator new (size_t n)
+	{
+		void* p = nullptr;
+		p = allocator<ListNode>().allocate(1);
+		cout << "memory pool allocate" << endl;
+		return p;
+	}
+};
+
+
+class Test
+{
+public:
+	int _data;
+
+	void Display()
+	{
+		cout << "Display()" << endl;
+	}
+};
+int main()
+{
+	ListNode l1;
+	l1._prev = nullptr;
+
+	Test t;
+	t._data = 11;
+	return 0;
+}
---
+++ Exercise/Exercise_3_30_2/Exercise_3_30_2/3_30_2c.c
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+//
+//struct ListNode
+//{
+//	struct ListNode* _next;
+//	struct ListNode* _prev;
+//
+//	void Display()
+//	{
+//		printf("Display()\n");
+//	}
+//};
