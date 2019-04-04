700e4a0e1f0a2b9b2f94fb1b56650acdc629db71
1705990245 <13991034472@139.com>
2019-04-04 22:37:43 +0800

    提交代码(4.4)

---
+++ Exercise/Exercise_4_4/Exercise_4_4/4_4.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+
+void Print(int* array, int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << array[i] << " ";
+	cout << endl;
+}
+void Swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int Parition(int* array, int left, int right)
+{
+	int begin = left;
+	int end = right - 1;
+	int key = array[right - 1];
+
+	while (begin < end)
+	{
+		while (begin < end && array[begin] <= key)
+			begin++;
+		while (begin < end && array[end] >= key)
+			end--;
+		if (begin < end)
+			Swap(&array[begin], &array[end]);
+	}
+	if (array[begin] != key)
+		Swap(&array[begin], &array[right - 1]);
+
+	return begin;
+}
+void Qsort(int* array, int left, int right)
+{
+	int mid = 0;
+	if (left < right)
+	{
+		mid = Parition(array, left, right);
+		Qsort(array, left, mid);
+		Qsort(array, mid + 1, right);
+	}
+}
+void MergeData(int* array, int left, int mid, int right,int* temp)
+{
+	int begin1 = left;
+	int end1 = mid;
+	int begin2 = mid;
+	int end2 = right;
+	int index = left;
+
+	while (begin1 < end1&& begin2 < end2)
+	{
+		if (array[begin1] < array[begin2])
+			temp[index++] = array[begin1++];
+		else
+			temp[index++] = array[begin2++];
+	}
+	while (begin1 < end1)
+		temp[index++] = array[begin1++];
+	while (begin2 < end2)
+		temp[index++] = array[begin2++];
+}
+void MergeSort(int* array, int size)
+{
+	int* temp = (int*)malloc(sizeof(int)* 10);
+	int gap = 1;
+	while (gap < size)
+	{
+		for (int i = 0; i < size; i += gap * 2)
+		{
+			int left = i;
+			int mid = left + gap;
+			int right = mid + gap;
+
+			if (mid > size)
+				mid = size;
+			if (right > size)
+				right = size;
+
+			MergeData(array, left, mid, right,temp);
+
+		}
+		memcpy(array, temp, sizeof(int)* 10);
+		gap *= 2;
+	}
+	free(temp);
+	temp = nullptr;
+}
+//int main()
+//{
+//	int array[10] = { 2, 5, 9, 0, 3, 6, 4, 8, 7, 1 };
+//	int size = 10;
+//	Print(array, size);
+//	//Qsort(array,0, size);
+//	MergeSort(array, size);
+//	Print(array, size);
+//
+//	return 0;
+//}
+int main()
+{
+	int* array = new int[10]{ 2, 5, 9, 0, 3, 6, 4, 8, 7, 1 };
+	int size = 10;
+	Print(array, size);
+	//Qsort(array,0, size);
+	MergeSort(array, size);
+	Print(array, size);
+	delete[] array;
+	return 0;
+}
+
+
