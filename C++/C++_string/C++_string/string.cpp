bb103941336965d098763e2066a5b6c01430b0c0
1705990245 <13991034472@139.com>
2019-04-15 22:31:41 +0800

    提交代码(4.15)

--- C++/C++_string/C++_string/string.cpp
+++ C++/C++_string/C++_string/string.cpp
@@ -2,33 +2,40 @@
 #include<iostream>
 using namespace std;
 
-namespace bit
+
+//第一种模拟方法：深拷贝(传统)
+namespace bit1
 {
 	class string
 	{
 	public:
-		string(const char* s = "")
+		//构造函数
+		string(const char* s = "") 
 		{
 			if (nullptr == s)
 				s = "";
+
+			_str = new char[strlen(s) + 1];
+			strcpy(_str, s);
 			_size = strlen(s);
 			_capacity = _size;
-			_str = new char[_capacity+1];
-			strcpy(_str, s);
 		}
+		//拷贝构造函数
 		string(const string& s)
-			:_str(new char[s._capacity + 1])
+			:_str(new char[s._capacity])
 			, _size(s._size)
 			, _capacity(s._capacity)	
 		{
 			strcpy(_str, s._str);
 		}
+		//赋值运算符重载函数
 		string& operator=(const string& s)
 		{
 			if (&s != this)
 			{
 				char* ptr = new char[s._capacity];
 				strcpy(ptr, s._str);
+
 				delete[] _str;
 				_str = ptr;
 				_capacity = s._capacity;
@@ -36,6 +43,7 @@ namespace bit
 			}
 			return *this;
 		}
+		//析构函数
 		~string()
 		{
 			if (_str)
@@ -52,18 +60,110 @@ namespace bit
 	};
 
 }
-
-void Teststring()
+void Teststring1()
 {
-	bit::string s1;
-	bit::string s2("hello");
-	bit::string s3(s2);
+	bit1::string s1;
+	bit1::string s2("hello");
+	bit1::string s3(s2);
 	s1 = s2;
+
 }
+//第二种模拟方法：深拷贝（现代）
+namespace bit2
+{
+	class string
+	{
+	public:
+		//构造函数
+		string(const char* s = "")
+		{
+			if (nullptr == s)
+				s = "";
+
+			_str = new char[strlen(s) + 1];
+			strcpy(_str, s);
+			_size = strlen(s);
+			_capacity = _size + 1;
+		}
+		//拷贝构造函数
+		string(const string& s)
+		{
+			string temp(s._str);
+			swap(_str, temp._str);//交换之后临时变量temp会带着旧的_str执行析构函数
+			_capacity = s._capacity;
+			_size = s._size;
+		}
+		//赋值运算符的重载函数
+		string& operator=(const string& s)
+		{
+			string temp(s._str);
+			swap(_str, temp._str);
+			_size = s._size;
+			_capacity = s._capacity;
+			return *this;
+		}
+		//析构函数
+		~string()
+		{
+			delete[] _str;
+			_size = 0;
+			_capacity = 0;
+		}
+	private:
+		char* _str;
+		size_t _capacity;
+		size_t _size;
+	};
+}
+void Teststring2()
+{
+	bit2::string s4(nullptr);
+	bit2::string s5("hello");
+	bit2::string s6(s5);
+	s4 = s5;
+}
+
+//第三种模拟方法：写时拷贝
+namespace bit
+{
+	class string
+	{
+	public:
+		string(const char* str = "") 
+		{
+			if (nullptr == str)
+				str = "";
+
+			_str = new char[strlen(str) + 1];
+			_size = strlen(str);
+			_capacity = _size + 1;
+		}
+		string(const string& s)
+		{
+
+		}
+		string& operator=(const string& s)
+		{
+
+		}
+		~string()
+		{
+			delete[] _str;
+			_size = 0;
+			_capacity = 0;
+		}
+	private:
+		char* _str;
+		size_t _size;
+		size_t _capacity;
+	};
+}
+
 
 int main()
 {
-	Teststring();
+	//Teststring1();
+	Teststring2();
 
 	return 0;
 }
