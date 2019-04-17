8d0e7940a51f864f995e341ec446a2577e2d1ffe
1705990245 <13991034472@139.com>
2019-04-17 22:15:17 +0800

    提交代码(4.17)

--- C++/C++_Date类/C++_Date类/Date.cpp
+++ C++/C++_Date类/C++_Date类/Date.cpp
@@ -18,7 +18,94 @@ Date::Date(const Date& d)
 	}
 	
 }
+// 判断年份y是否为润年
+static bool IsLeapyear(int y)
+{
+	if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
+		return true;
+	return false;
+}
+// 年份y的天数
+static int GetYearDays(int y)
+{
+	if (IsLeapyear(y))
+		return 366;
+	return 365;
+}
+// 日期d当前月份的天数
+static int GetMonthDays(const Date &d)
+{
+	int n;						
 
+	switch (d.GetMonth())
+	{
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+		n = 31;		
+		break;
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		n = 31;						
+		break;
+	case 2:
+		if (IsLeapyear(d.GetYear())) n = 29;	
+		else n = 28;							
+	}
+
+	return n;
+}
+// 返回从公元1年1月1日起的天数
+static int DateToNum(const Date &d)
+{
+	int y = 0;
+	int m = 1;
+	int n = 0;
+
+	for (y = 0; y < d.GetYear(); y++)
+		n += GetYearDays(y);
+	for (m = 1; m < d.GetMonth();m++)
+		n += GetMonthDays(Date(y,m,1));
+	
+	n += d.GetDay();
+	return n;
+}
+// 由从公元1年1月1日起的天数返回日期
+static Date NumToDate(int n)
+{
+	int year = 0; 
+	int month = 0;
+	int rest = n;
+
+	for (year = 1; rest > GetYearDays(year); year++)
+		rest -= GetYearDays(year);
+
+	for (month = 1; rest > GetMonthDays(Date(year, month, 1)); month++)
+		rest -= GetMonthDays(Date(year, month, 1));
+
+	 Date d(year, month, rest);
+	return d;
+}
+ostream &operator<<(ostream &out, const Date &d)
+{
+	out << d.GetYear() << "/" << d.GetMonth() << "/" << d.GetDay();
+	return out;
+}
+istream &operator>>(istream &in, Date &d)
+{
+	int year;
+	int month;
+	int day;
+	cin >> year >> month >> day;
+	d = Date(year, month, day);
+	return in; 
+}
 
 //重载函数
 Date& Date::operator=(const Date& d)
@@ -30,38 +117,41 @@ Date& Date::operator=(const Date& d)
 }
 Date Date::operator+(int days)
 {
-	_day += days;
-	return *this;
+	int day = DateToNum(*this) + days;
+	return NumToDate(day);
 }
 Date Date::operator-(int days)
 {
-	_day -= days;
-	return *this;
+	int day = DateToNum(*this) - days;
+	return NumToDate(day);
 }
 int Date::operator-(const Date& d)
 {
-
+	int day = DateToNum(*this) - DateToNum(d);
+	return day;
 }
 Date& Date::operator++()
 {
-	_day++;
-	return *this;
+	int day = DateToNum(*this);
+	return NumToDate(++day);
 }
 Date Date::operator++(int)
 {
 	Date temp(*this);
-	_day++;
+	int day = DateToNum(*this);
+	*this = NumToDate(++day);
 	return temp;
 }
 Date& Date::operator--()
 {
-	_day--;
-	return *this;
+	int day = DateToNum(*this);
+	return NumToDate(--day);
 }
 Date Date::operator--(int)
 {
 	Date temp(*this);
-	_day--;
+	int day = DateToNum(*this);
+	*this = NumToDate(--day);
 	return temp;
 
 }
--- C++/C++_Date类/C++_Date类/Date.h
+++ C++/C++_Date类/C++_Date类/Date.h
@@ -4,9 +4,25 @@ using namespace std;
 
 class Date
 {
+	friend ostream &operator<<(ostream &out, const Date &d);		// 重载输出运算符
+	friend istream &operator>>(istream &in, Date &d);
 public:
 	Date(int year = 1900, int month = 1, int day = 1);
 	Date(const Date& d);
+
+	void SetYear(int y){ _year = y; }
+	void SetMonth(int m){ _month = m; }// 设置月 
+	void SetDay(int d){ _day = d; }		// 设置日
+	int GetYear() const{ return _year; }	// 返回年 
+	int GetMonth() const{ return _month; }	// 返回月 
+	int GetDay() const{ return _day;  }	// 返回日
+	static bool IsLeapyear(int y);			// 判断年份y是否为润年
+	static int GetYearDays(int y);			// 年份y的天数
+	static int GetMonthDays(const Date &d);	// 日期d当前月份的天数
+	static int DateToNum(const Date &d);		// 返回从公元1年1月1日起的天数
+	static Date NumToDate(int n);			// 由从公元1年1月1日起的天数返回日期
+
+
 	Date& operator=(const Date& d);
 	Date operator+(int days);
 	Date operator-(int days);
