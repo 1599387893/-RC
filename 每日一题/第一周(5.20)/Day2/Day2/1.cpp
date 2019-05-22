2802e07e75ca4e3063f4cd540552a7a2148933ec
1705990245 <13991034472@139.com>
2019-05-22 20:11:07 +0800

    Push_5_22

---
+++ 每日一题/第一周(5.20)/Day2/Day2/1.cpp
@@ -0,0 +1,38 @@
+//1
+//排序子序列(排序子序列为非递增或者非递减的序列，本质上是求一段序列中的波峰和波谷的个数)
+//输入示例：
+//			6					||4					||6
+//			1 2 3 2 2 5			||1 3 2 3			||1 2 3 3 2 1
+//输出示例：
+//			2					||2					||2
+
+#if 0
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	vector<int> v;
+	v.resize(n);
+	for (size_t i = 0; i < n; ++i)
+		cin >> v[i];
+
+	int ret = 1;
+	for (size_t i = 1; i < n-2; ++i)
+	{
+		if ((v[i]>=v[i - 1] && v[i] >= v[i + 1]) || (v[i] <= v[i - 1] && v[i] <= v[i + 1]))
+		{
+			++ret;
+			if (i != n - 3)
+				++i;
+		}
+	}
+	cout << ret << endl;
+
+	system("pause");
+	return 0;
+}
+#endif
---
+++ 每日一题/第一周(5.20)/Day2/Day2/2.cpp
@@ -0,0 +1,49 @@
+//2
+//倒置字符串（将一段字符串中的单词前后交换，以单词为单位）
+//输入示例：
+//			i like beijing
+//输出示例：
+//			beijing like i
+
+//方法一
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+	
+	reverse(s.begin(), s.end());
+	for (int i = 0; i < s.size(); ++i)
+	{
+		int j = i;
+		while(s[j] != ' '&& j<s.size())
+			++j;
+		reverse(&s[i],&s[j]);
+		i = j;
+	}
+	cout << s << endl;
+	system("pause");
+	return 0;
+}
+
+#if 0
+//方法二
+#include <iostream>
+#include <string>
+using namespace std;
+
+// cin读取string时自动会被空格分隔开，用另一个字符串存储进行逆序输出
+int main()
+{
+	string s1, s2;
+	cin >> s2;
+	while (cin >> s1)
+		s2 = s1 + " " + s2;
+	cout << s2 << endl;
+	return 0;
+}
+#endif
