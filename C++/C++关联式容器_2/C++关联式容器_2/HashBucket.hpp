268760bccb1f44d05c245a95c925f07629e67abe
1705990245 <13991034472@139.com>
2019-07-04 21:32:32 +0800

    Update_7_4

--- C++/C++关联式容器_2/C++关联式容器_2/HashBucket.hpp
+++ C++/C++关联式容器_2/C++关联式容器_2/HashBucket.hpp
@@ -7,11 +7,11 @@ using namespace std;
 template<class T>
 struct HashBucketNode
 {
-	HashBucket(const T& value)
+	HashBucketNode(const T& value)
 		:_pNext(nullptr)
 		, _val(value)
 	{}
-	HashBucket<T>* _pNext;
+	HashBucketNode<T>* _pNext;
 	T _val;
 };
 
@@ -22,22 +22,130 @@ class HashBucket
 	typedef Node* PNode;
 public:
 	HashBucket(const size_t& capacity = 10)
-		:_hb(capacity)
+		:_table(capacity)
 		, _size(capacity)
 	{}
-	bool Insert(const T& data)
+	~HashBucket()
 	{
-
+		Clear();
 	}
-	PNode* Find(const T& data)
+	bool Insert(T& data)
 	{
-
+		//0.检测是否需要扩容
+		CheckCapacity();
+		//1.计算当前元素所在的桶号
+		size_t bucketNo = HashFunc(data);
+		//2.检测该元素是否存在
+		PNode pCur = _table[bucketNo];
+		while (pCur)
+		{
+			if (pCur->_val == data)
+				return false;
+			pCur = pCur->_pNext;
+		}
+		//3.插入元素（类似链表的头插）
+		pCur = new Node(data);
+		pCur->_pNext = _table[bucketNo];
+		_table[bucketNo] = pCur;
+	}
+	PNode Find(const T& data)
+	{
+		size_t bucket = HashFunc(data);
+		PNode pCur = _table[bucket];
+		while (pCur)
+		{
+			if (pCur->_val == data)
+				return pCur;
+			pCur = pCur->_pNext;
+		}
+		return nullptr;
 	}
 	bool Erase(const T& data)
 	{
-
+		size_t bucket = HashFunc(data);
+		PNode pCur = _table[bucket];
+		PNode pPer = nullptr;
+		while (pCur)
+		{
+			//删除链表中的第一个结点
+			if (pCur->_val == data)
+			{
+				if (pPer == nullptr)
+					_table[bucket] = pCur->_pNext;
+				else
+					pPer->_pNext = pCur->_pNext;
+				delete pCur;
+				pCur = nullptr;
+				--_size;
+				return true;
+			}
+			pPer = pCur;
+			pCur = pCur->_pNext;
+		}
+		return false;
+	}
+	size_t size()
+	{
+		return _size;
 	}
 private:
-	vector<PNode<T>> _hb;
+	void CheckCapacity()
+	{
+		//当有效元素个数等于桶的个数时扩容
+		if (_size == _table.capacity())
+		{
+			HashBucket<T> newHB(_size * 2);
+			for (size_t i = 0; i < _table.capacity(); ++i)
+			{
+				PNode pCur = _table[i];
+				while (pCur)
+				{
+					//1.计算当前节点在新哈希桶中的桶号
+					size_t newBucketNo = newHB.HashFunc(pCur->_val);
+					
+					//2.将pCur从旧哈希桶中移除
+					_table[i] = pCur->_pNext;
+					
+					//3.将pCur头插到新哈希桶中
+					pCur->_pNext = newHB._table[newBucketNo];
+					newHB._table[newBucketNo] = pCur;
+					
+					//4.从旧哈希桶中取出下一个结点
+					pCur = _table[i];
+				}
+			}
+			_table.swap(newHB._table);
+			swap(newHB._size, _size);
+		}
+	}
+	size_t HashFunc(T& data)
+	{
+		return data % _table.capacity();
+	}
+	void Clear()
+	{
+		for (int i = 0; i < _table.capacity(); ++i)
+		{
+			PNode pCur = _table[i];
+			while (pCur)
+			{
+				_table[i] = pCur->_pNext;
+				delete pCur;
+				pCur = _table[i];
+			}
+		}
+	}
+private:
+	vector<HashBucketNode<T>*> _table;
 	size_t _size;
 };
+
+void TestHashBucket()
+{
+	HashBucket<int> hb(10);
+	cout << hb.size() << endl;
+	int array[] = { 3, 8, 4, 0, 7, 13, 33 };
+	for (auto& e : array)
+		hb.Insert(e);
+	cout << hb.size() << endl;
+}
--- C++/C++关联式容器_2/C++关联式容器_2/HashTable.hpp
+++ C++/C++关联式容器_2/C++关联式容器_2/HashTable.hpp
@@ -1,10 +1,8 @@
 #pragma once
-#include<vector>
 #include<iostream>
+#include<vector>
 using namespace std;
-
-//假设：哈希表格中的元素具有唯一性
-
+//插入元素具有唯一性(删除位置不能再次插入元素)
 enum State {EMPTY,EXIST,DELETE};
 
 template<class T>
@@ -14,77 +12,110 @@ struct Elem
 	State _state;
 };
 
-template<class T>
+template<class T,bool IsLine = true>
 class HashTable
 {
 public:
 	HashTable(size_t capacity = 10)
 		:_ht(capacity)
 		, _size(0)
-	{
-		for (auto& e : _ht)
-			e._state = EMPTY;
-	}
-
+	{}
 	//插入
 	bool Insert(const T& data)
 	{
-		//检测是否需要扩容
-		
-		//求出哈希地址
+		//0.检测是否需要扩容
+		CheckCapacity();
+		//1.通过哈希函数计算元素在哈希表中的存储位置
 		size_t hashadds = HashFunc(data);
-		
-		//确定该位置为空（EMPYTY）即可插入
+		int i = 0;
+
+		//2.检测该位置是否可以直接插入元素
+		//如果发生哈希冲突
 		while (_ht[hashadds]._state != EMPTY)
 		{
-			//如果该结点存在，返回false
+			//如果该元素存在，返回false
 			if (_ht[hashadds]._state == EXIST && _ht[hashadds]._val == data)
 				return false;
+			//若该位置被占用，则向后找下一个空位置（线性探测）
+			if (IsLine)
+			{
+				hashadds++;
+				if (hashadds = _ht.capacity())
+					hashadds = 0;
+			}
+			else//二次探测
+			{
+				hashadds = hashadds + 2 * i + 1;
+				hashadds %= _ht.capacity();
+			}
 
-			++hashadds;
-
-			if (hashadds == _ht.capacity())
-				hashadds = 0;
 		}
-		
-		_ht[hashadds]._state = EXIST;
+		//插入元素
 		_ht[hashadds]._val = data;
-		++_size;
-
+		_ht[hashadds]._state = EXIST;
+		_size++;
 		return true;
 	}
-	int Find(const T& data)
+	//查找
+	size_t Find(const T& data)
 	{
 		size_t hashadds = HashFunc(data);
-		//如果该位置的标记不为空（EXIST,DELETE）继续循环
+		int i = 0;
 		while (_ht[hashadds]._state != EMPTY)
 		{
 			if (_ht[hashadds]._state == EXIST && _ht[hashadds]._val == data)
 				return hashadds;
 
-			hashadds++;
-			if (hashadds == _ht.capacity())
-				hashadds = 0;
-		}
 
+			//（线性探测--缺陷：容易发生数据堆积--解决方法：二次探测--H(i+1) = H(i) + i ^ 2 + 1）
+			if (IsLine)
+			{
+				++hashadds;
+				if (hashadds = _ht.capacity())
+					hashadds = 0;
+			}
+			else//二次探测（缺陷：当有效元素个数逐渐接近容量时，找到空位置的概率会降低；空间利用率低）
+			{
+				hashadds = hashadds + 2 * i + 1;
+				hashadds %= _ht.capacity();
+			}
+		}
 		return -1;
 	}
+	//删除
 	bool Erase(const T& data)
 	{
-		int pos = Find(data);
-		if (-1 != pos)
+		size_t hashadds = Find(data);
+		if (-1 != hashadds)
 		{
-			_ht[pos]._state = DELETE;
+			_ht[hashadds]._state = DELETE;
+			_size--;
 			return true;
 		}
 		return false;
-
 	}
-
 private:
-	size_t HashFunc(const T& val)
+	//扩容函数
+	void CheckCapacity()
 	{
-		return val % _ht.capacity();
+		//负载因子：有效元素个数/哈希表容量
+		if (_size * 10 / _ht.capacity() >= 7)
+		{
+			HashTable<T> Newht(2 * _ht.capacity());
+			//将原哈希表中存在的元素搬移到新哈希表中；
+			for (int i = 0; i < _ht.capacity(); ++i)
+			{
+				if (_ht[i]._state == EXIST)
+					Newht.Insert(_ht[i]._val);
+			}
+			_ht.swap(Newht._ht);
+			swap(_size, Newht._size);
+		}
+	}
+	//哈希函数
+	int HashFunc(int data)
+	{
+		return data % _ht.capacity();
 	}
 private:
 	vector<Elem<T>> _ht;
@@ -94,12 +125,11 @@ private:
 void TestHashTable()
 {
 	HashTable<int> ht;
-	int array[] = { 1, 4, 5, 6, 7, 44, 9 };
+	int array[] = { 4,7,8,27,9,5,3,1};
 	for (auto& e : array)
 		ht.Insert(e);
 
-	cout<<ht.Erase(5)<<endl;
-	cout<<ht.Find(44)<<endl;
+	ht.Erase(5);
 	ht.Insert(34);
 
 }
