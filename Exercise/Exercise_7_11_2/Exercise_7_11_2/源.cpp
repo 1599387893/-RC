6ab744c72bb4bbc02714b3ec729edbdd0ec97665
1705990245 <13991034472@139.com>
2019-07-12 22:46:49 +0800

    Exercise_7_12

---
+++ Exercise/Exercise_7_11_2/Exercise_7_11_2/源.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct BinaryTreeNode
+{
+	int _val;
+	BinaryTreeNode* _pLeft;
+	BinaryTreeNode* _pRight;
+};
+int main()
+{
+	vector<int> pPer{1,2,4,7,3,5,6,8};
+	vector<int> pIn{4,7,2,3,5,3,8,6};
+
+}
