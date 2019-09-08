cf8657997c6e47f118042192e4b41fbc45f3a663
1705990245 <13991034472@139.com>
2019-09-08 18:12:36 +0800

    Java基本类型大小，类型转换，对象和类

---
+++ JAVA/TestTypeSize.java
@@ -0,0 +1,70 @@
+public class TestTypeSize{
+	public static void main(String[] args){
+		//byte
+		System.out.println("byte 二进制位数："+Byte.SIZE);
+		System.out.println("包装类：java.lang.Byte");
+		System.out.println("最小值：Byte.MIN_VALUE=" + Byte.MIN_VALUE);
+		System.out.println("最大值：Byte.MAX_VALUE=" + Byte.MAX_VALUE);
+		
+		//short
+		System.out.println("short 二进制位数："+Short.SIZE);
+		System.out.println("包装类：java.lang.Short");
+		System.out.println("最小值：Short.MIN_VALUE" + Short.MIN_VALUE);
+		System.out.println("最大值：Short.MAX_VALUE" + Short.MAX_VALUE);
+		
+		//int
+		System.out.println("Integer 二进制位数："+Integer.SIZE);
+		System.out.println("包装类：java.lang.Integer");
+		System.out.println("最小值：Integer.MIN_VALUE" + Integer.MIN_VALUE);
+		System.out.println("最大值：Integer.MAX_VALUE" + Integer.MAX_VALUE);
+		
+		//Long
+		System.out.println("Long 二进制位数："+Long.SIZE);
+		System.out.println("包装类：java.lang.Long");
+		System.out.println("最小值：Long.MIN_VALUE" + Long.MIN_VALUE);
+		System.out.println("最大值：Long.MAX_VALUE" + Long.MAX_VALUE);
+
+		//float
+		System.out.println("float 二进制位数："+Float.SIZE);
+		System.out.println("包装类：java.lang.float");
+		System.out.println("最小值：float.MIN_VALUE" + Float.MIN_VALUE);
+		System.out.println("最大值：float.MAX_VALUE" + Float.MAX_VALUE);		
+
+		//double
+		System.out.println("double 二进制位数："+Double.SIZE);
+		System.out.println("包装类：java.lang.double");
+		System.out.println("最小值：double.MIN_VALUE" + Double.MIN_VALUE);
+		System.out.println("最大值：double.MAX_VALUE" + Double.MAX_VALUE);		
+
+		//char
+		System.out.println("char 二进制位数："+Character.SIZE);
+		System.out.println("包装类：java.lang.char");
+		System.out.println("最小值：char.MIN_VALUE" + (int)Character.MIN_VALUE);
+		System.out.println("最大值：char.MAX_VALUE" + (int)Character.MAX_VALUE);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
