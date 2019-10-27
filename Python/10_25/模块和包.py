d7934c13742a833ded0695169ece5c9288f8f35b
1705990245 <13991034472@139.com>
2019-10-27 23:08:56 +0800

    完善模块和包

--- Python/10_25/模块和包.py
+++ Python/10_25/模块和包.py
@@ -16,10 +16,28 @@ for path in sys.path:
 # --------------模块也是一个对象，也有生命周期和使用范围
 #---------------import是导入整个模块
 #---------------from import是导入模块中指定的名字的部分
+'''
 from os.path import exists
-
 exists("f:/test.txt")
+'''
 
 #---------------多次import同一个模块，只会导入一次
 #---------------导入一次模块，相当于执行一次模块
 
+#模块的内置变量
+#print(globals())
+
+#------------------------------------------------------包---------------------------------
+# 所谓的包就是一个特殊的目录
+# 包里面必须包含一个 __init__.py 文件
+# 这个文件间负责对包进行初始化
+'''
+import sys
+print(sys.argv)
+print(len(sys.argv))
+print(sys.executable)
+print(sys.platform)
+'''
+import os
+#print(os.getenv("PATH"))
+print(os.uname())
