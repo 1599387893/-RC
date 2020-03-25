fe10be53f5597f94d10266d6536c718439dbd5f3
1705990245 <13991034472@139.com>
2020-03-25 21:20:24 +0800

    排序，测试

---
+++ Exercise/3_25/3_25/测试缓冲区溢出.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char arr[10] = "";
+	strcpy(arr,"123456789115646546546897916679877");
+	printf("%s\n",arr);
+	//system("pause");
+	scanf("%c",arr[0]);
+	return 0;
+}
